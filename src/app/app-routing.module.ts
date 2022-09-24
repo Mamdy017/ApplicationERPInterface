5434c7d4a6ba55c58bf3f1c44b1a1e837b26da6b
tddiarra <tiecouradiarra1999@gmail.com>
2022-09-24 21:28:07 +0000

    Composant Gestion entite realiser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -75,7 +75,11 @@ const routes: Routes = [
   {
     path: 'salle',
     loadChildren: () => import('./salle/salle.module').then( m => m.SallePageModule)
+  },  {
+    path: 'gestionentite',
+    loadChildren: () => import('./gestionentite/gestionentite.module').then( m => m.GestionentitePageModule)
   },
+
 ];
 
 @NgModule({
