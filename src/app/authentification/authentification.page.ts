9b4f982e9dcf192268a2e70cbade2c18bfe6b529
tddiarra <tiecouradiarra1999@gmail.com>
2022-09-26 09:15:53 +0000

    La gestion des personnelles redirection admin,responsable, user

--- src/app/authentification/authentification.page.ts
+++ src/app/authentification/authentification.page.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 import { AuthentificationService } from '../Services/authentification/authentification.service';
+import { Router } from '@angular/router';
+import { Utilisateur } from '../modeles/utilisateur/utilisateur';
 
 @Component({
   selector: 'app-authentification',
@@ -15,10 +17,22 @@ export class AuthentificationPage implements OnInit {
   email:any;
   password:any;
   erreur:any;
+  typeUser:any;
+
+
+
+  utilisateur: Utilisateur = {
+    iduser:0,
+    nom:'',
+    prenom:'',
+    numero:'',
+    email:'',
+    password:'',
+  }
 
   bool_erreur: boolean = false;
   
-  constructor(private service : AuthentificationService, private http:HttpClient) { }
+  constructor(private service : AuthentificationService, private http:HttpClient, private route:Router) { }
 
   ngOnInit() {
   
@@ -31,15 +45,47 @@ export class AuthentificationPage implements OnInit {
       this.erreur="Tous les champs sont obligatoires";
       console.log(this.erreur);
     }else{
-      this.service.seConnecter(this.email,this.password).subscribe(data=>{
-        this.connexion = data;
-        console.log(data);
-        this.erreur = data.contenu; 
-      })
-    } 
+        this.service.seConnecter(this.email,this.password).subscribe(data=>{
+          this.connexion = data;
 
+          console.log(data);
+          
+          if(this.connexion.email == this.email && this.connexion.password==this.password)
+          {
+           //On recupere le type de role de l'utilisateur en question
+            this.typeUser = data.role.nom;   
+            
+            if(this.typeUser != null)
+              {
+                      if(this.typeUser == "User")
+                      {
+                      this.route.navigateByUrl('/accueil-user');
+                      }
+                      else if(this.typeUser == "Admin")
+                      {
+                      this.route.navigateByUrl('/ajouter-postulant');
+                        
+                      }
+                      else
+                      {
+                        this.route.navigateByUrl('');
+                      }
+              }
+          }
+           //On recupere le type de role de l'utilisateur en question
+          // console.log("Utilisateur = "+this.typeUser)
+          
+          // ajouter-postulant
+          // this.route.navigateByUrl('/ajouter-postulant');accueil-user
+          // routerLink="/ajouter-postulant"
+          this.erreur = data.contenu;
+
+          
+        })
+      }
+      
+    } 
+ 
     
   }
   
- 
-}
