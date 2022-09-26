e5ca02c170583c20ca3bc4be6e3eac2a250941c0
tddiarra <tiecouradiarra1999@gmail.com>
2022-09-26 04:08:56 +0000

    Page authentification liée à l'interface

--- src/app/authentification/authentification.page.ts
+++ src/app/authentification/authentification.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { AuthentificationService } from '../Services/authentification/authentification.service';
 
 @Component({
   selector: 'app-authentification',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthentificationPage implements OnInit {
 
-  constructor() { }
+  connexion:any;
+  email:any;
+  password:any;
+  erreur:any;
+
+  bool_erreur: boolean = false;
+  
+  constructor(private service : AuthentificationService, private http:HttpClient) { }
 
   ngOnInit() {
+  
+    
   }
 
+  Connexion(){
+    this.bool_erreur = true;
+    if(typeof this.email === 'undefined' || typeof this.password === 'undefined'){
+      this.erreur="Tous les champs sont obligatoires";
+      console.log(this.erreur);
+    }else{
+      this.service.seConnecter(this.email,this.password).subscribe(data=>{
+        this.connexion = data;
+        console.log(data);
+        this.erreur = data.contenu; 
+      })
+    } 
+
+    
+  }
+  
+ 
 }
