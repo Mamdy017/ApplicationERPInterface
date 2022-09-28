af6af4a0e0c0eadc6ede9368d194806bbdd807ce
Youssouf DJIRE <yddjire@odc.local>
2022-09-28 10:24:32 +0000

    liste-des-salles

---
+++ src/app/liste-des-salles/liste-des-salles.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-des-salles',
+  templateUrl: './liste-des-salles.page.html',
+  styleUrls: ['./liste-des-salles.page.scss'],
+})
+export class ListeDesSallesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
