d3f7bbbe811848b4825151a516b66012bcb7f609
adcoulibaly <coulibalyadamabekaye03@gmail.com>
2022-10-03 11:38:00 +0000

    Pour se placer sur la branche de Mamdy 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterLinkActive } from '@angular/router';
 import { LayoutModule } from '@angular/cdk/layout';
 
 
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,7 +24,8 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLinkActive
     
     // LayoutModule,
   ],
