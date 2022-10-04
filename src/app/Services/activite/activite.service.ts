b19fec24d2eb91743283e09cb209828efb98c1cf
adcoulibaly <coulibalyadamabekaye03@gmail.com>
2022-10-04 07:34:49 +0000

    Activité mis a jours

--- src/app/Services/activite/activite.service.ts
+++ src/app/Services/activite/activite.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { from, Observable } from 'rxjs';
+import { Activite } from 'src/app/modeles/activite/activite';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +15,35 @@ export class ActiviteService {
   recupererListeActiviteT():Observable<any>{
     return this.http.get(`${this.api}/activite/afficher`);
   }
+
+// ==========================AJOUT DE L'Activite
+
+postActivite(idacteurs: string, idacteurInternes: string, libelleEntite: string, typeAct: string, libelleSalle: string, activite:Activite):Observable<any>{
+
+  console.log(`${this.api}/activite/ajouter/${idacteurs}/${idacteurInternes}/${libelleEntite}/${typeAct}/${libelleSalle}`, activite)
+
+  return this.http.post(`${this.api}/activite/ajouter/${idacteurs}/${idacteurInternes}/${libelleEntite}/${typeAct}/${libelleSalle}`, activite);
+}
+
+// ===========================
+
+recupererListeEntite():Observable<any>{
+  return this.http.get(`${this.api}/entite/afficher`);
+}
+
+recupererListeSalle():Observable<any>{
+  return this.http.get(`${this.api}/salle/afficher`);
+}
+
+recupererListeTypeActivite():Observable<any>{
+  return this.http.get(`${this.api}/typeactivite/afficher`);
+}
+
+recupererListeAnne():Observable<any>{
+  return this.http.get(`${this.api}/annee/afficher`)
+}
+
+
+
+
 }
--- src/app/activite/activite.page.ts
+++ src/app/activite/activite.page.ts
@@ -1,5 +1,8 @@
-import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Activite } from '../modeles/activite/activite';
+import { TypeActivite } from '../modeles/type-activite/type-activite';
+import { ActiviteService } from '../Services/activite/activite.service';
+import { ListeActeurService } from '../services/liste-acteur/liste-acteur.service';
 
 @Component({
   selector: 'app-activite',
@@ -7,35 +10,117 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activite.page.scss'],
 })
 export class ActivitePage implements OnInit {
-  menuBureau: boolean = true;
-  menuMobile: boolean = false;  
+
+  constructor(private activiteService : ActiviteService,
+    private utilisateurs: ListeActeurService) { }
 
 
-  constructor( public breakpointObserver: BreakpointObserver) { }
-  actualise(): void {
-    setInterval(
-      () => {
-      }, 100, clearInterval(1500));
+  salles$:any;
+  annees$:any;
+  typeActivites$:any;
+  entites$:any;
+  users$:any;
+  acteurs$:any;
+  respons$:any
+
+  titre:any
+  salles:any;
+  annees:any;
+  typeActivites:any;
+  entites:any;
+  users:any;
+  acteurs:any;
+  descpt:any;
+  dateDebut:any;
+  dateFin:any;
+  respons:any
+  nombrepersonnedemande:any;
+
+
+  activiteObjet: Activite = {
+    nom: "",
+    description: "",
+    dateDebut: new Date(),
+    dateFin: new Date(),
+  nombrepersonnedemande:0
+
   }
+
+ 
+
+  getListeEntite(){
+    this.activiteService.recupererListeEntite().subscribe((data) =>{
+      this.entites$ = data;
+    })
+  }
+
+  getListeSalle(){
+    this.activiteService.recupererListeSalle().subscribe((data) =>{
+      this.salles$ = data;
+    })
+  }
+
+  getListeTypeActivite(){
+    this.activiteService.recupererListeTypeActivite().subscribe((data) =>{
+      this.typeActivites$ = data
+    })
+  }
+
+  getListeAnnee(){
+    this.activiteService.recupererListeAnne().subscribe((data) =>{
+      this.annees$ = data;
+    })
+  }
+
+  getListeUsers(){
+    this.utilisateurs.lesUtilisateurs().subscribe((data) =>{
+      this.users$ = data
+      this.respons$ = data
+    })
+  }
+
+  getActeurs(){
+    this.utilisateurs.afficherLesActeurs().subscribe((data) =>{ 
+      this.acteurs$ = data;
+    })
+  }
+
+  postActivite(){
+    this.activiteObjet.nom = this.titre;
+    this.activiteObjet.dateDebut = this.dateDebut;
+    this.activiteObjet.dateFin = this.dateFin;
+    this.activiteObjet.description = this.descpt;
+    this.activiteObjet.nombrepersonnedemande = this.nombrepersonnedemande;
+
+    console.log("les acteurs internes: " + this.users);
+    console.log("les acteurs internes: " + this.acteurs);
+    console.log("Les acteurs externes" + this.salles)
+    console.log("Les acteurs externes" + this.entites)
+    console.log("Les acteurs externes" + this.acteurs)
+    console.log("Les acteurs externes" + this.descpt)
+    console.log("titre: " + this.titre);
+    console.log("a" + this.dateDebut);
+    console.log("da: "+ this.dateFin);
+    console.log("type: " + this.typeActivites)
+    console.log("resp: " + this.respons)
+
+    this.activiteService.postActivite(this.acteurs, this.users, this.entites, this.typeActivites, this.salles, this.activiteObjet).subscribe((data) =>{
+      console.log("Donnée envoyé avec succes");
+    });
+  }
+  
+
   ngOnInit() {
-    this.breakpointObserver
-      .observe(['(max-width: 767px)'])
-      .subscribe((state: BreakpointState) => {
-        if (state.matches) {
-          this.menuBureau = false;
-          this.menuMobile = true;
-          this.actualise();
-        } else {
-          this.menuBureau = true;
-          this.menuMobile = false;
-          this.actualise();
-        }
-      });
+
+    this.getListeSalle();
+    this.getListeEntite();
+    this.getListeTypeActivite();
+    this.getListeAnnee();
+    this.getListeUsers();
+    this.getActeurs();
+
   }
 
-  afficheMenuMobile() {
-    this.menuBureau = true;
-    this.menuMobile = false;
-  }
+ 
 
 }
