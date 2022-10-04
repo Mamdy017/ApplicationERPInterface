f241baf3868f2f6576d5cdfcaca9d4b60f4ec031
KEITA Mahamadou <kmahamadou858@gmail.com>
2022-10-04 06:02:36 +0000

    ajout de l'activite non complet

--- src/app/Services/activite/activite.service.ts
+++ src/app/Services/activite/activite.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { from, Observable } from 'rxjs';
+import { Activite } from 'src/app/modeles/activite/activite';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,28 @@ export class ActiviteService {
 
   constructor(private http: HttpClient) { }
 
+  postActivite(idacteurs: string, idacteurInternes: string,  libelleEntite: string, typeAct: string, libelleSalle: string, activite:Activite):Observable<any>{
+
+    return this.http.post(`${this.api}/activite/ajouter/${idacteurs}/${idacteurInternes}/${libelleEntite}/${typeAct}/${libelleSalle}`, activite);
+  }
+
   recupererListeActiviteT():Observable<any>{
     return this.http.get(`${this.api}/activite/afficher`);
+  }
+
+  recupererListeEntite():Observable<any>{
+    return this.http.get(`${this.api}/entite/afficher`);
+  }
+
+  recupererListeSalle():Observable<any>{
+    return this.http.get(`${this.api}/salle/afficher`);
+  }
+
+  recupererListeTypeActivite():Observable<any>{
+    return this.http.get(`${this.api}/typeactivite/afficher`);
+  }
+
+  recupererListeAnne():Observable<any>{
+    return this.http.get(`${this.api}/annee/afficher`)
   }
 }
