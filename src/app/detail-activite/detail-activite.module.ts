baddd4c882d16445015434bb12f5456bfcb1b2ce
jkkoloma <kolomajeremi60@gmail.com>
2022-10-10 19:11:30 +0000

    ReDesign de la page detail-activite

--- src/app/detail-activite/detail-activite.module.ts
+++ src/app/detail-activite/detail-activite.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DetailActivitePageRoutingModule } from './detail-activite-routing.module';
 
 import { DetailActivitePage } from './detail-activite.page';
+import { MenuComponent } from '../menu/menu.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { DetailActivitePage } from './detail-activite.page';
     IonicModule,
     DetailActivitePageRoutingModule
   ],
-  declarations: [DetailActivitePage]
+  declarations: [DetailActivitePage,MenuComponent]
 })
 export class DetailActivitePageModule {}
--- src/app/detail-activite/detail-activite.page.ts
+++ src/app/detail-activite/detail-activite.page.ts
@@ -1,4 +1,6 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail-activite',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailActivitePage implements OnInit {
 
-  constructor() { }
+  constructor(  public breakpointObserver: BreakpointObserver,  private route: Router) { }
   // /==============================================================================SESSION==========
   iduser:any;
   roles:any;
@@ -15,10 +17,29 @@ export class DetailActivitePage implements OnInit {
   prenom_users:any;
  email_users: string;
  numero_users: string;
+ menuBureau: boolean = true;
+ menuMobile: boolean = false;
 // /+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-
+actualise(): void {
+  setInterval(
+    () => {
+    }, 100, clearInterval(1500));
+}
   ngOnInit() {
+    this.breakpointObserver
+    .observe(['(max-width: 767px)'])
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.menuBureau = false;
+        this.menuMobile = true;
+        this.actualise();
+      } else {
+        this.menuBureau = true;
+        this.menuMobile = false;
+        this.actualise();
+      }
+    });
     // ===========================================================================SESSION VALEURS================================================
 this.iduser =  sessionStorage.getItem("id_users");
 this.roles = sessionStorage.getItem("role_users"); 
@@ -26,6 +47,16 @@ this.noms_users =  sessionStorage.getItem("nom_users");
 this.prenom_users = sessionStorage.getItem("prenom_users",);
 this.email_users = sessionStorage.getItem("email_users");
 this.numero_users = sessionStorage.getItem("numero_users");
+  }
+
+  deconnexion() {
+    sessionStorage.clear();
+    console.log('je suis le log')
+    this.route.navigateByUrl('/authentification');
+  }
+  afficheMenuMobile() {
+    this.menuBureau = true;
+    this.menuMobile = false;
   }
 
 }
