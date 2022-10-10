86ba907127de861236858273b49c7f969531db4e
adkonte <alykonte19@gmail.com>
2022-10-10 17:08:22 +0000

    commit du lundi 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy, RouterLinkActive } from '@angular/router';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -13,9 +14,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
+import {DynamicDialogModule} from 'primeng/dynamicdialog';
 import{NgChartsModule} from 'ng2-charts'
 import { Ng2SearchPipe, Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 // import { Ng2SearchPipeModule } from 'ng2-search-filter';
@@ -27,12 +29,14 @@ import { Ng2SearchPipe, Ng2SearchPipeModule } from 'ng2-search-filter';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     RouterLinkActive,
     Ng2SearchPipeModule,
     NgChartsModule,
+    NgbModule
     
 
     // FilterPipe
--- src/app/liste-globale/liste-globale.module.ts
+++ src/app/liste-globale/liste-globale.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -15,7 +16,8 @@ import { NgxPaginationModule } from 'ngx-pagination';
     CommonModule,
     FormsModule,
     IonicModule,
-    ListeGlobalePageRoutingModule, NgxPaginationModule
+    ListeGlobalePageRoutingModule, NgxPaginationModule,
+    ReactiveFormsModule
   ],
   declarations: [ListeGlobalePage,MenuComponent]
 })
